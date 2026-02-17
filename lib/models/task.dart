065121318c44a13ba509eba453a341c55d5164cc
naiput <uptowngirl@Naila-Putri.local>
2026-02-17 08:53:11 +0700

    creating model

---
+++ lib/models/task.dart
@@ -0,0 +1,18 @@
+class Task {
+  final String name;
+  final String priority;
+  final int duration;
+  final String deadline;
+
+  Task({
+    required this.name,
+    required this.priority,
+    required this.duration,
+    required this.deadline,
+  });
+
+  @override
+  String toString() {
+    return 'task {name: $name, priority:$priority, duration:$duration, deadline:$deadline }';
+  }
+}
